0582de4e551e435dcb092932fcf4f05610df86db
Kamel Faour <10121641@mu.edu.lb>
2025-05-12 00:08:55 +0300

    Post Provider Done

---
+++ lib/providers/post_provider.dart
@@ -0,0 +1,40 @@
+import 'package:coursetrack/data/db_helper.dart';
+import 'package:coursetrack/models/blog_post.dart';
+import 'package:flutter/foundation.dart';
+
+class PostProvider extends ChangeNotifier {
+  final DbHelper _dbHelper = DbHelper();
+  List<BlogPost> _posts = [];
+
+  List<BlogPost> get posts => _posts;
+
+  Future<void> fetchAllPosts() async {
+    _posts = await _dbHelper.getAllPosts();
+    notifyListeners();
+  }
+
+  Future<void> fetchUserPosts(String author) async {
+    _posts = await _dbHelper.getMyPosts(author);
+    notifyListeners();
+  }
+
+  Future<void> addPost(BlogPost post) async {
+    await _dbHelper.insertPost(post);
+    await fetchAllPosts(); // Refresh the list after adding
+  }
+
+  Future<void> updatePost(BlogPost post) async {
+    await _dbHelper.updatePost(post);
+    await fetchAllPosts(); // Refresh the list after updating
+  }
+
+  Future<void> deletePost(int id) async {
+    await _dbHelper.deletePost(id);
+    await fetchAllPosts(); // Refresh the list after deleting
+  }
+
+  Future<void> searchPosts(String keyword) async {
+    _posts = await _dbHelper.searchPosts(keyword);
+    notifyListeners();
+  }
+}
